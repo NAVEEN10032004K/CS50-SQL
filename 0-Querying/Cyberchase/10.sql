6de693e18b38e631044c077108bfd7bfc0d77e4b
Kenchgunde Naveen <63925474+NAVEEN10032004K@users.noreply.github.com>
2025-03-18 00:21:05 +0530

    Delete 0-Querying/Cyberchase directory

--- 0-Querying/Cyberchase/10.sql
+++
@@ -1,6 +0,0 @@
--- In 10.sql, write a SQL query to answer a question of your choice about the prints. The query should:
--- Make use of AS to rename a column
--- Involve at least one condition, using WHERE
--- Sort by at least one column, using ORDER BY
-SELECT MAX(contrast) AS "Maximum Contrast", artist, brightness FROM views
-WHERE artist = "Hiroshige"  ORDER BY brightness;
